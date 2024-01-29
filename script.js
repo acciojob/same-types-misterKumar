d0500c59131f07668ee30a47aab1baafc6483b53
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 20:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function isSameType(value1, value2) {
-  return typeof(value1)===typeof(value2)
-
+  //your js code here
+	let a=typeof(value1);
+	let b =typeof(value2);
+	if(value1==NaN && value2==NaN){
+		return true;
+	}
+	else if(a===b && a!=NaN && b!=NaN){
+		return true;
+	}else{
+		return false;
+	}
 }
 
-// // do not change the code below.
+// do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
